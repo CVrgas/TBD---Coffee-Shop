cd68bc6147aaec6a3c3704c0627be23430b86146
CVrgas <usual389@gmail.com>
2026-04-07 20:52:28 -0400

    refactor(IReadRepository): update ListAsync and CountAsync methods to accept nullable specifications

--- src/Application/Common/Abstractions/Persistence/Repository/IReadRepository.cs
+++ src/Application/Common/Abstractions/Persistence/Repository/IReadRepository.cs
@@ -9,8 +9,8 @@ public interface IReadRepository<T, TKey> where T : class, IEntity<TKey>
     Task<T?> GetByIdAsync(TKey id, bool asNoTracking = true, CancellationToken ct = default);
     Task<T?> GetAsync(ISpecification<T> spec, bool asNoTracking = true, CancellationToken ct = default);
     Task<TProjection?> GetAsync<TProjection>(ISpecification<T> spec, Expression<Func<T, TProjection>> selector, bool asNoTracking = true, CancellationToken ct = default);
-    Task<IEnumerable<T>> ListAsync(ISpecification<T> spec, bool asNoTracking = true, CancellationToken ct = default);
-    Task<int> CountAsync(ISpecification<T> spec, bool asNoTracking = true, CancellationToken ct = default);
+    Task<IList<T>> ListAsync(ISpecification<T>? spec = null, bool asNoTracking = true, CancellationToken ct = default);
+    Task<int> CountAsync(ISpecification<T>? spec = null, bool asNoTracking = true, CancellationToken ct = default);
     Task<Paginated<T>> PaginatedAsync(ISpecification<T> spec, CancellationToken ct = default);
     Task<bool> ExistsAsync(ISpecification<T> specification, bool asNoTracking = true, CancellationToken ct = default);
 }
