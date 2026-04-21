eee4a621f7c307b616d5c0b7e57ad97c693611e4
CVrgas <usual389@gmail.com>
2026-04-21 18:20:19 -0400

    refactor(Inventory): remove repository abstractions in AdjustStock command handler; implement DbContext directly for stock adjustments and introduce GetStockLevel query handler

--- src/Application/Inventory/Dtos/StockLevelDto.cs
+++ src/Application/Inventory/Dtos/StockLevelDto.cs
@@ -5,6 +5,6 @@ public record StockLevelDto
     public int ProductId { get; init; }
     public int QuantityInStock { get; init; }
     public int ReservedQuantity { get; init; }
-    public int AvailableQuantity { get; init; }
+    public int AvailableQuantity => QuantityInStock - ReservedQuantity;
     public List<LocationStockLevelDto> LocationStockLevels { get; init; } = [];
 }
--- src/Application/Inventory/Specifications/StockLevelByProductIdSpec.cs
+++
@@ -1,6 +0,0 @@
-using Application.Common.Interfaces;
-using Domain.Inventory;
-
-namespace Application.Inventory.Specifications;
-
-public class StockLevelByProductIdSpec(int productId) : Specification<StockItem>(s => s.ProductId == productId && s.IsActive);
