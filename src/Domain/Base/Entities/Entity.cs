49914c15253a06b3c578a8de41fdd792ebb2c851
CVrgas <usual389@gmail.com>
2026-05-30 19:16:00 -0400

    feat(Entity): add domain event handling to Entity base class; implement domain events collection and equality operators

--- src/Domain/Base/Entities/Entity.cs
+++ src/Domain/Base/Entities/Entity.cs
@@ -1,11 +1,30 @@
+using Domain.Base.Events;
+
 namespace Domain.Base.Entities;
 
 public abstract class Entity<TKey> : IEntity<TKey> where TKey : notnull
 {
     public TKey Id { get; protected set; } = default!;
+
+    private readonly List<IDomainEvent> _domainEvents = [];
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+    public void ClearDomainEvents() => _domainEvents.Clear();
+
+    public override bool Equals(object? obj) =>
+        obj is Entity<TKey> other && Id.Equals(other.Id);
+
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public static bool operator ==(Entity<TKey>? left, Entity<TKey>? right) =>
+        left is null ? right is null : left.Equals(right);
+
+    public static bool operator !=(Entity<TKey>? left, Entity<TKey>? right) =>
+        !(left == right);
 }
 
 public abstract class EntityWithRowVersion<TKey> : Entity<TKey>, IHasRowVersion where TKey : notnull
 {
-    public byte[] RowVersion { get; set; } = null!;
+    public byte[] RowVersion { get; protected set; } = null!;
 }
